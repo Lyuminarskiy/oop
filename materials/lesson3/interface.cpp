a9e501dea149c78fdeb6400286e6a00a17df691f
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2018-03-02 20:31:16 +0400

    Доработки примеров для задания №3.

--- materials/lesson3/interface.cpp
+++ materials/lesson3/interface.cpp
@@ -18,5 +18,61 @@ class Shape : public Drawable
 
 int main()
 {
-	return 0;
+    return 0;
 }
+
+class Person {}
+class Student : public Person {}
+class Teacher : public Person {}
+
+0..1
+1
+0..* или *
+1..*
+
+class Group { }
+
+class Student {
+  public:
+    Group* group;
+    Student()
+}
+
+class Body {}
+class Head {}
+
+class Human {
+  public:
+    Body* body;
+    Head* head;
+
+    Human() {
+      body = new Body();
+      head = new Head();
+    }
+
+    ~Human() {
+      delete body;
+      delete head;
+    }
+}
+
+class Hat {}
+class Pants {}
+class Shirt {}
+
+class Human2 {
+  public:
+    Hat* hat;
+    Pants* pants;
+    Shirt* shirt;
+
+    Human(Hat* hat, Pants* pants, Shirt* shirt)
+    {
+      this->hat = hat;
+      this->pants = pants;
+      this->shirt = shirt;
+    }
+}
+
+Visual Paradigm
