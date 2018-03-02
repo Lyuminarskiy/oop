2f2567f180850b845b145c85ee617290e47128d2
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2018-03-03 00:08:59 +0400

    Доработал пример кода для третьего занятия.

--- materials/lesson3/abstract.cpp
+++ materials/lesson3/abstract.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include <iostream>
 
 using namespace std;
@@ -6,14 +5,81 @@ using namespace std;
 // Абстрактный класс
 class Shape
 {
-    string name;
+  string name;
 
-  public:
-    virtual void draw() = 0;
-    virtual string getName() { return name; }
-}
+public:
+  virtual void draw() = 0;
+  virtual string getName() { return name; }
+};
+
+// Интерфейс
+struct Drawable
+{
+  virtual void draw() = 0;
+};
+
+class Shape : public Drawable
+{
+  string name;
+
+public:
+  virtual string getName() { return name; }
+};
+
+// Ассоциация
+class Person {};
+class Student : public Person {};
+class Teacher : public Person {};
+
+class Group {};
+struct Student2
+{
+  Group* group;
+  Student2();
+};
+
+// Композиция
+class Body {};
+class Head {};
+class Human
+{
+  Body* body;
+  Head* head;
+
+public:
+  Human()
+  {
+    body = new Body();
+    head = new Head();
+  }
+
+  ~Human()
+  {
+    delete body;
+    delete head;
+  }
+};
+
+// Аргегация
+class Hat {};
+class Pants {};
+class Shirt {};
+class Human2
+{
+  Hat* hat;
+  Pants* pants;
+  Shirt* shirt;
+
+public:
+  Human2(Hat* hat, Pants* pants, Shirt* shirt)
+  {
+    this->hat = hat;
+    this->pants = pants;
+    this->shirt = shirt;
+  }
+};
 
 int main()
 {
-	return 0;
+  return 0;
 }
