3b2b5420a2bf842353fab839eacc2dd02681f6f3
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2019-04-01 16:01:18 +0400

    Добавлена заготовка примера для занятия №3.

---
+++ docs/practice/03/assets/solid/solid/srp/srp1_after.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace solid.srp_after
+{
+  interface IPrinter
+  {
+    void Print(string text);
+  }
+
+  class ConsolePrinter : IPrinter
+  {
+    public void Print(string text) =>
+      Console.WriteLine(text);
+  }
+
+  class PdfPrinter : IPrinter
+  {
+    public void Print(string text) { /* ... */ }
+  }
+
+  class HtmlPrinter : IPrinter
+  {
+    public void Print(string text) { /* ... */ }
+  }
+
+  class Report
+  {
+    public string Text { get; set; }
+
+    public void GoToFirstPage() =>
+      Console.WriteLine("Переход к первой странице");
+
+    public void GoToLastPage() =>
+      Console.WriteLine("Переход к последней странице");
+
+    public void GoToPage(int pageNumber) =>
+      Console.WriteLine($"Переход к странице {pageNumber}");
+
+    public void Print(IPrinter printer) =>
+      printer.Print(this.Text);
+  }
+
+  class Program
+  {
+    static void Test()
+    {
+      var report = new Report
+      {
+        Text = "Текст отчёта"
+      };
+
+      IPrinter printer = new ConsolePrinter();
+      report.Print(printer);
+    }
+  }
+}
---
+++ docs/practice/03/assets/solid/solid/srp/srp1_before.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace solid.srp_before
+{
+  class Book
+  {
+    public string Text { get; set; }
+    public void GoToFirstPage()
+    {
+      Console.WriteLine("Переход к первой странице");
+    }
+
+    public void GoToLastPage()
+    {
+      Console.WriteLine("Переход к последней странице");
+    }
+
+    public void GoToPage(int pageNumber)
+    {
+      Console.WriteLine("Переход к странице {0}", pageNumber);
+    }
+
+    public void Print()
+    {
+      Console.WriteLine("Печать отчета");
+      Console.WriteLine(Text);
+    }
+  }
+
+  class Program
+  {
+    static void Test()
+    {
+      var report = new Book
+      {
+        Text = "Текст отчёта"
+      };
+
+      report.Print();
+    }
+  }
+}
